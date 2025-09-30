9e524d4ac454d601c55c8f1344a30123d8862369
jbardnz <hello@jarrad.dev>
2025-10-01 10:14:54 +1300

    Remove without overlapping

--- routes/console.php
+++ routes/console.php
@@ -5,5 +5,4 @@ use Illuminate\Support\Facades\Schedule;
 // Scheduled Tasks
 Schedule::command('check:availability')
     ->hourly()
-    ->withoutOverlapping()
     ->appendOutputTo(storage_path('logs/scheduler.log'));
